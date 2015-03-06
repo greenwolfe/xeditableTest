a0164d52a3542c5d6564064fcaab7740ff3a07d9
Matt Greenwolfe <solargreen@mindspring.com>
2015-03-06 15:27:50 -0500

    minimal code to produce bugs with x-editable in wysihtml mode

---
+++ xeditableTest.js
@@ -0,0 +1,16 @@
+if (Meteor.isClient) {
+
+  Template.xedit.rendered = function () {
+    $('#textArea.editable').editable({
+      success: function(response,newValue) {
+        console.log(newValue);
+      },
+      wysihtml5: {
+        "color":true //,
+        //"lists":true,
+        //"link": true
+      }
+    });
+  }
+
+}
